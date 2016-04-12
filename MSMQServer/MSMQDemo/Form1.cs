54039cb5fbdf09e468d6493306105c907fdbc48f
Amarantin <wangyayun.its@foxmail.com>
2016-04-12 14:36:11 +0800

    commit project

---
+++ MSMQServer/MSMQDemo/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MSMQDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Open queue
+            MessageQueue queue = new System.Messaging.MessageQueue(".\\Private$\\MSMQDemo");
+
+            // Create message
+            System.Messaging.Message message = new System.Messaging.Message();
+            message.Body = txtMessage.Text.Trim();
+            message.Formatter = new System.Messaging.XmlMessageFormatter(new Type[] { typeof(string) });
+
+            // Put message into queue
+            queue.Send(message);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Open queue
+            MessageQueue queue = new System.Messaging.MessageQueue(".\\Private$\\MSMQDemo");
+
+            // Receive message, 同步的Receive方法阻塞当前执行线程，直到一个message可以得到
+            System.Messaging.Message message = queue.Receive();
+            message.Formatter = new System.Messaging.XmlMessageFormatter(new Type[] { typeof(string) });
+            txtReceiveMessage.Text = message.Body.ToString();
+        }
+    }
+}
---
+++ MSMQServer/MSMQServer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Messaging;
+
+namespace MSMQServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 创建一个私有消息队列
+            if (!MessageQueue.Exists(@".\Private$\LearningHardMSMQ"))
+            {
+                using (MessageQueue mq = MessageQueue.Create(@".\Private$\LearningHardMSMQ"))
+                {
+                    mq.Label = "LearningHardPrivateQueue";
+                    Console.WriteLine("已经创建了一个私有队列");
+                    Console.WriteLine("路径为:{0}", mq.Path);
+                    Console.WriteLine("私有队列名字为:{0}", mq.QueueName);
+                    mq.Send("MSMQ Private Message", "Leaning Hard"); // 发送消息
+                }
+            }
+
+            if (MessageQueue.Exists(@".\Private$\LearningHardMSMQ"))
+            {
+                // 获得私有消息队列
+                MessageQueue mq = new MessageQueue(@".\Private$\LearningHardMSMQ");
+                mq.Send("Sending MSMQ private message" + DateTime.Now.ToLongDateString(), "Leaning Hard");
+                Console.WriteLine("Private Message is sent to {0}", mq.Path);
+            }
+
+            Console.Read();
+
+        }
+    }
+}
